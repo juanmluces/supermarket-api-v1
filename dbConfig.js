f39e668c5374719aa2e0005a9e82eec87cdd1a59
Juan Miguel Luces Rincon <juanmluces@gmail.com>
2024-08-31 19:25:45 +0200

    update mysql to mysql2

--- dbConfig.js
+++ dbConfig.js
@@ -1,16 +1,16 @@
-const mySql = require('mysql');
+const mySql = require("mysql2");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const createPool = () => {
-  const pool = mySql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-  });
-  global.db = pool
-}
+	const pool = mySql.createPool({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT,
+		database: process.env.DB_NAME,
+	});
+	global.db = pool;
+};
 
-module.exports = { createPool }
+module.exports = { createPool };
